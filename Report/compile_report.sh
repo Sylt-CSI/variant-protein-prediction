6575b9062fbff7670dfd90498528cb570ccd674b
Sylt-CSI <s.s.schuurmans@st.hanze.nl>
2019-06-11 14:30:27 +0200

    Layout change and inserted references.

---
+++ Report/compile_report.sh
@@ -0,0 +1,4 @@
+pdflatex -synctex=1 -interaction=nonstopmode Protein_variant_prediction
+biber Protein_variant_prediction
+pdflatex -synctex=1 -interaction=nonstopmode Protein_variant_prediction
+pdflatex -synctex=1 -interaction=nonstopmode Protein_variant_prediction
